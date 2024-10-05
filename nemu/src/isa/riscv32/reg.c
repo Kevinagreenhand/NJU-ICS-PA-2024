4bf6384fc2d1a8090f2b22993b2a132eebf6bbf9
tracer-ics2024 <tracer@njuics.org>
2024-10-05 22:53:25 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:53:25 up 10:14,  1 user,  load average: 0.63, 0.35, 0.32

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
 	for(int i=0;i<32;i++)
-		printf("%s  \t%x  \t%d",regs[i],cpu.gpr[i],cpu.gpr[i]);
+		printf("%s  \t%x  \t%d\n",regs[i],cpu.gpr[i],cpu.gpr[i]);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
