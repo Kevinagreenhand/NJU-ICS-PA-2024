b133ed1763aeda238e44abd708ed75fd890e0fdc
tracer-ics2024 <tracer@njuics.org>
2024-10-12 19:25:52 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:25:52 up  3:12,  1 user,  load average: 0.54, 0.51, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -205,8 +205,7 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-  int rec=eval(0,nr_token-1);
-  return rec;//changed
+  return eval(0,nr_token-1);//changed
   /* TODO: Insert codes to evaluate the expression. */
   
 
