cca9ce89b8b37f12388aac256e14f7ae3fa4b7dc
tracer-ics2024 <tracer@njuics.org>
2024-11-11 23:58:04 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:58:04 up  1:07,  1 user,  load average: 1.22, 1.34, 1.20

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -24,7 +24,7 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, // none
+  TYPE_N, //TYPE_J// none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
@@ -32,7 +32,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-
+//#define immJ() do {*imm=}
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
   int rs1 = BITS(i, 19, 15);
@@ -42,6 +42,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    //case TYPE_J:
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
@@ -68,6 +69,8 @@ static int decode_exec(Decode *s) {
   //begins
   INSTPAT("??????? ????? ????? ??? ????? 01101 11",lui,U,R(rd)=imm);
   INSTPAT("??????? ????? ????? 000 ????? 00100 11",addi,I,R(rd)=src1+imm);
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", li   , I, R(rd) = src1 + imm);
+  //INSTPAT("??????? ????? ????? 000 ????? 11011 11",jal,J,R(rd)=s->pc+4;s->dnpc=s->pc+imm);
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
