06ca335cad6a39f4d4ba7debcc1f671e4362abe0
tracer-ics2024 <tracer@njuics.org>
2024-11-22 19:56:20 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:56:20 up 20:54,  1 user,  load average: 1.15, 0.83, 0.71

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -65,7 +65,13 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  char *sglcha=(char *)s;
+  while(n!=0){
+    *sglcha=c;
+    n--;
+    sglcha++;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
