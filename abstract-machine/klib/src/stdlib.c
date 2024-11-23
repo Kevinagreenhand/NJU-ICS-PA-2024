ce82cdfb0dd2862334c12fff230ffbab1798f27d
tracer-ics2024 <tracer@njuics.org>
2024-11-23 21:36:54 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:36:54 up 1 day, 22:34,  1 user,  load average: 0.47, 0.68, 0.83

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,7 +4,8 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
-
+static char* start_addr;    // addr的初始值
+static bool init_flag = 0;  // 初始化的标志, 初始化完成后置1
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
@@ -30,14 +31,16 @@ int atoi(const char* nptr) {
 }
 
 void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+if(!init_flag) {
+        start_addr = (void*)ROUNDUP(heap.start, 8);
+        init_flag = true;
+    }
+    size = (size_t)ROUNDUP(size, 8);
+    char* old = start_addr; // 获取addr
+    start_addr += size;
+    return old; // [addr, addr + size]
 }
+
 
 void free(void *ptr) {
 }
