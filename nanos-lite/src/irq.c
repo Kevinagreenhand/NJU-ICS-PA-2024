ea9f9de5237c02844a3b5707afd40b17ab7029d3
tracer-ics2024 <tracer@njuics.org>
2024-12-24 00:27:47 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:27:47 up 0 min,  1 user,  load average: 0.46, 0.15, 0.05

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD : Log("yield succeed!"); c->mepc += 4; break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
