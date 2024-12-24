485494ee54233512e32eb4b68206472407eda829
tracer-ics2024 <tracer@njuics.org>
2024-12-24 20:10:38 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:10:38 up  9:59,  1 user,  load average: 0.70, 0.47, 0.57

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,6 +10,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
   //case SYS_exit:halt(0);break;
   case 1:yield();c->GPRx=0;break;
+  case 0:halt(0);break;
   default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
