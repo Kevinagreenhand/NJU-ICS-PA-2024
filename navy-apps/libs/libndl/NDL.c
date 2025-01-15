7a560adf09380af89e4bf5a13acd2812a6a30657
tracer-ics2024 <tracer@njuics.org>
2025-01-15 16:49:33 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:49:33 up  6:31,  1 user,  load average: 1.67, 1.57, 1.28

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -66,12 +66,6 @@ int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
-  printf("ready ro open");
-  int screen_fd = open("proc/dispinfo",0,0);
-  char buf[64];
-  read(screen_fd, buf, 64);
-  sscanf(buf, "WIDTH:%d\nHEIGHT:%d\n", &screen_w, &screen_h);
-  printf("screen_w=%d, screen_h=%d\n", screen_w, screen_h);
   return 0;
 }
 
