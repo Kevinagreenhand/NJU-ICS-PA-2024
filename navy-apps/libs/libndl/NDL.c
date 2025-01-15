7f60d38a27f7d62541b592743e995b3668be4f7e
tracer-ics2024 <tracer@njuics.org>
2025-01-15 21:26:41 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:41 up 11:09,  1 user,  load average: 1.09, 1.20, 1.18

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -47,7 +47,6 @@ void NDL_OpenCanvas(int *w, int *h) {
   }
   if(*w==0&&*h==0)
     *w = screen_w; *h = screen_h;
-  printf("777777777777");
   canvas_w = *w; canvas_h = *h;
     
 }
@@ -55,7 +54,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = open("/dev/fb", 0, 0);
   for (int i = 0; i < h && y + i < canvas_h; ++i) {
-    lseek(fd, ((y + (screen_h-canvas_h)/2 + i) * screen_w + (x + (screen_w-canvas_w)/2)) * 4-150, SEEK_SET);
+    lseek(fd, ((y + (screen_h-canvas_h)/2 + i) * screen_w + (x + (screen_w-canvas_w)/2)) * 4, SEEK_SET);
     write(fd, pixels + i * w, 4 * (w < canvas_w - x ? w : canvas_w - x));
   }
 
