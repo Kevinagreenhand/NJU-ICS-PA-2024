3e148c795a215e45329d4043d36812e2c5d46c67
tracer-ics2024 <tracer@njuics.org>
2025-01-15 11:02:12 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:02:12 up 44 min,  1 user,  load average: 0.67, 0.75, 0.64

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  int ms = 500;
   uint32_t tmp=0;
   int flag=0;
   while (1) {
     tmp=NDL_GetTicks();
     if (tmp%500==0&&flag==0) {
     printf("time %d\n ", tmp);
-    ms += 500;
     flag= 1;
-  }
-  if (tmp%500==0&&flag==1) {
-    printf("time %d\n ", tmp);
-    flag= 0;
   }
   if(tmp%500!=0){
     flag=0;
