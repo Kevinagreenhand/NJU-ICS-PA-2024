d6c8efa5d209b9defcd343f969f4e56a9a3f352a
tracer-ics2024 <tracer@njuics.org>
2025-01-15 21:57:23 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:57:23 up 11:39,  1 user,  load average: 2.22, 1.44, 1.19

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -63,6 +63,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
+  printf("offset=%d,len=%d\n",offset,len);
   int width = ev.width;
   offset /= 4;
   len /= 4;
