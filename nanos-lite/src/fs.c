b4e59917e5db8d46bc4ef891f3d2193364acfa97
tracer-ics2024 <tracer@njuics.org>
2025-01-15 21:21:06 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:21:06 up 11:03,  1 user,  load average: 1.29, 1.23, 1.18

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -77,7 +77,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 
 size_t fs_write(int fd, const void *buf, size_t len){
   if(file_table[fd].write!=NULL){
-    return file_table[fd].write(buf,0,len);
+    return file_table[fd].write(buf,file_table[fd].open_offset,len);
   }
   if(file_table[fd].open_offset+len>file_table[fd].size){
       len=file_table[fd].size-file_table[fd].open_offset;
