2a7e9e6148b4eab5660269fecc2bca82ac6c9b81
tracer-ics2024 <tracer@njuics.org>
2025-01-16 12:46:54 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:46:54 up  2:27,  1 user,  load average: 1.96, 1.87, 1.46

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -21,7 +21,7 @@ int SDL_PollEvent(SDL_Event *ev) {
     return 0;
   }
   sscanf(buf, "%s %s %d\n", type, key_name, &keycode);
-  printf("I'm here\n");
+  printf("%s\n",keycode);
   ev->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
   ev->key.keysym.sym = keycode;
   return 1; 
