06fa23691f51a8dfe4b7041ba812070bc8af6acd
tracer-ics2024 <tracer@njuics.org>
2025-01-16 15:28:39 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:39 up  5:09,  1 user,  load average: 1.61, 1.59, 1.39

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   switch_boot_pcb();
-  naive_uload(NULL, "/bin/timer-test");
+  naive_uload(NULL, "/bin/bird");
   Log("Initializing processes...");
   // load program here
 
