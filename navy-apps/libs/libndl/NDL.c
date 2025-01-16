aa3f4c7b48dc0f35535a5b99b71ed801a5f7e426
tracer-ics2024 <tracer@njuics.org>
2025-01-16 12:44:23 +0800

    >  compile NEMU 231098216 田博文 Linux KevinsDell 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:44:23 up  2:24,  1 user,  load average: 1.23, 1.65, 1.33

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -21,7 +21,6 @@ int NDL_PollEvent(char *buf, int len) {
   //注意，这里应该是直接open。若错，后改
   int fd = open("/dev/events", 0, 0);
   int tmp = read(fd, (void*)buf, len);
-  printf("%s\n",buf);
   if(tmp==0)
     return 0;
   else
